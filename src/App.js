55770af743cf144d39a944efae791519c7f9889d
Mohammad Touhidul Islam <muhdtohidul@gmail.com>
2021-05-08 11:27:46 +0600

    Added Redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import Main from './Components/Main';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Main />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Main />
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
