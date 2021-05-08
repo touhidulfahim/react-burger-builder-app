9035b23d522fdddf54fbb9703973abfef2c7e991
Mohammad Touhidul Islam <muhdtohidul@gmail.com>
2021-05-08 16:20:12 +0600

    Saving Orders to Database

--- src/Components/Orders/Checkout/Checkout.js
+++ src/Components/Orders/Checkout/Checkout.js
@@ -1,5 +1,20 @@
 import React, { Component } from 'react';
 import { Button } from 'reactstrap';
+import { connect } from 'react-redux';
+import axios from 'axios';
+
+
+
+const mapStateToProps = state => {
+    return {
+        ingredients: state.ingredients,
+        totalPrice: state.totalPrice,
+        purchasable: state.totalPrice
+    }
+}
+
+
+
 
 class Checkout extends Component {
     state = {
@@ -24,12 +39,27 @@ class Checkout extends Component {
     }
 
     submitHandler = () => {
-        console.log(this.state.values);
+        const order = {
+            ingredients: this.props.ingredients,
+            customer: this.state.values,
+            price: this.state.totalPrice,
+            orderTime: new Date(),
+        }
+        axios.post("https://foodishapp-e638b-default-rtdb.firebaseio.com/orders.json", order)
+            .then(response => console.log(response))
+            .catch(err => console.log(err))
+        console.log(order);
     }
 
     render() {
         return (
             <div>
+                <h4 style={{
+                    border: "1px solid grey",
+                    boxShadow: "1px 1px #888888",
+                    borderRadius: "5px",
+                    padding: "20px",
+                }}>Payment:{this.props.totalPrice} BDT</h4>
                 <form style={{
                     border: "1px solid grey",
                     boxShadow: "1px 1px #888888",
@@ -53,4 +83,4 @@ class Checkout extends Component {
     }
 }
 
-export default Checkout;
+export default connect(mapStateToProps)(Checkout);
