f10c4b46461ad0583d1fd7c68f10c295190147be
Mohammad Touhidul Islam <muhdtohidul@gmail.com>
2021-05-16 21:14:36 +0600

    Added Spinner for after login

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -3,13 +3,22 @@ import { Formik } from 'formik';
 
 import { connect } from 'react-redux';
 import { auth } from '../../redux/authActionCreators';
-
+import Spinner from '../Spinner/Spinner';
 
 const mapDispatchToProps = dispatch => {
     return {
         auth: (email, password, mode) => dispatch(auth(email, password, mode))
     }
 }
+
+const mapStateToProps = state => {
+    return {
+        authLoading: state.authLoading,
+        authFailedMsg: state.authFailedMsg,
+    }
+}
+
+
 class Auth extends Component {
     state = {
         mode: "Sign Up"
@@ -20,8 +29,11 @@ class Auth extends Component {
     }
 
     render() {
-        return (
-            <div>
+        let form = null;
+        if (this.props.authLoading) {
+            form = <Spinner />
+        } else {
+            form = (
                 <Formik
                     initialValues={
                         {
@@ -89,6 +101,7 @@ class Auth extends Component {
                                     name="password"
                                     placeholder="Password"
                                     className="form-control"
+                                    type="password"
                                     value={values.password}
                                     onChange={handleChange}
                                 />
@@ -99,6 +112,7 @@ class Auth extends Component {
                                     <input
                                         name="passwordConfirm"
                                         placeholder="Confirm Password"
+                                        type="password"
                                         className="form-control"
                                         value={values.passwordConfirm}
                                         onChange={handleChange}
@@ -111,9 +125,14 @@ class Auth extends Component {
                             </form>
                         </div>)}
                 </Formik>
+            )
+        }
+        return (
+            <div>
+                {form}
             </div>
         )
     }
 }
 
-export default connect(null, mapDispatchToProps)(Auth);
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
